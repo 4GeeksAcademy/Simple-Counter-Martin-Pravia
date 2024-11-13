3d124ab9684dfe8771983daf182645d683b19e4b
Martín Pravia <marto.pravia@gmail.com>
2024-11-13 17:20:02 +0000

    creo Reloj.jsx, dentro el html para maquetar

---
+++ src/js/component/Reloj.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Reloj = () => {
+  return (
+    <div className="container">
+      <div className="watch">
+        <i class="fa-regular fa-clock"></i>
+      </div>
+      <div className="hour1">Hora1</div>
+      <div className="hour2">Hora2</div>
+      <div className="minute1">Minuto1</div>
+      <div className="minute2">Minuto2</div>
+      <div className="second1">Segundo1</div>
+      <div className="second">Segundo2</div>
+    </div>
+  );
+};
+
+export default Reloj;
