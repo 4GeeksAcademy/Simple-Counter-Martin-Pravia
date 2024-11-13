a43c2761de6f726988797420e320475fc050936d
Martín Pravia <marto.pravia@gmail.com>
2024-11-13 20:22:58 +0000

    Cargo SegundosActuales con sus props, se pasan a reloj

--- src/js/component/Reloj.jsx
+++ src/js/component/Reloj.jsx
@@ -1,30 +1,36 @@
-import {useState, useEffect} from "react";
-import React from 'react';
+import { useState, useEffect } from "react";
+import React from "react";
+import SegundosActuales from "./SegundosActuales";
 
 const Reloj = () => {
-const [segundos, setSegundos] = useState(0);
+  const [segundos, setSegundos] = useState(0);
 
-useEffect(() =>{
-  const tiempo = setTiempo(() => {
-    setSegundos((segundosAnteriores) => segundosAnteriores +1);
-  }, 1000);
-  
-}
-)
- 
-}
+  useEffect(() => {
+    const tiempo = setInterval(() => {
+      setSegundos((segundosAnteriores) => segundosAnteriores + 1);
+    }, 1000);
+    return () => clearInterval(tiempo);
+  }, []);
+
+  const second1 = Math.floor(seconds % 10);
+  const second2 = Math.floor((seconds / 10) % 10);
+  const second3 = Math.floor((seconds / 100) % 10);
+  const second4 = Math.floor((seconds / 1000) % 10);
+  const second5 = Math.floor((seconds / 10000) % 10);
+  const second6 = Math.floor((seconds / 100000) % 10);
 
   return (
     <div className="container">
       <div className="watch">
         <i className="fa fa-clock"></i>
       </div>
-      <div className="s6">S6</div>
-      <div className="s5">S5</div>
-      <div className="s4">S4</div>
-      <div className="s3">S3</div>
-      <div className="s2">S2</div>
-      <div className="s1">S1</div>
+      <SegundosActuales numeroAColocar={second1} />
+      <SegundosActuales numeroAColocar={second2} />
+      <SegundosActuales numeroAColocar={second3} />
+      <SegundosActuales numeroAColocar={second4} />
+      <SegundosActuales numeroAColocar={second5} />
+      <SegundosActuales numeroAColocar={second6} />
+   
     </div>
   );
 };
---
+++ src/js/component/SegundosActuales.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SegundosActuales = ({numeroAColocar}) => {
+    return <div className='numeroDeSegundos'>{numeroAColocar}</div>;
+};
+
+export default SegundosActuales;
